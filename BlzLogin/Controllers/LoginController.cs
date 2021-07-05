50a80c9b8e791ab01cc066832ea82aa50a73bebd
Moises Aguilar <moises.aguilar@ideewe.com>
2021-07-05 11:48:10 -0600

    Initial Version
    Version Inicial Code

---
+++ BlzLogin/Controllers/LoginController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.DirectoryServices;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlzLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost("/account/login")]
+        public async Task<IActionResult> Login(UserCredentials credentials)
+        {
+            //Indicamos el dominio en el que vamos a buscar al usuario
+            string path = "LDAP://servidor.dominio.local/dc=servidor,dc=dominio,dc=local";
+
+            try
+            {
+                using (DirectoryEntry entry = new DirectoryEntry(path, credentials.Username, credentials.Password))
+                {
+                    using (DirectorySearcher searcher = new DirectorySearcher(entry))
+                    {
+                        //Buscamos por la propiedad SamAccountName
+                        searcher.Filter = "(samaccountname=" + credentials.Username + ")";
+                        //Buscamos el usuario con la cuenta indicada
+                        var result = searcher.FindOne();
+                        if (result != null)
+                        {
+                            string role = "";
+                            //Comporbamos las propiedades del usuario
+                            ResultPropertyCollection fields = result.Properties;
+                            foreach (String ldapField in fields.PropertyNames)
+                            {
+                                foreach (Object myCollection in fields[ldapField])
+                                {
+                                    if (ldapField == "employeetype")
+                                        role = myCollection.ToString().ToLower();
+                                }
+                            }
+
+                            //Añadimos los claims Usuario y Rol para tenerlos disponibles en la Cookie
+                            //Podríamos obtenerlos de una base de datos.
+                            var claims = new[]
+                            {
+                                new Claim(ClaimTypes.Name, credentials.Username),
+                                new Claim(ClaimTypes.Role, role)
+                            };
+
+                            //Creamos el principal
+                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                            //Generamos la cookie. SignInAsync es un método de extensión del contexto.
+                            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+                            //Redirigimos a la Home
+                            return LocalRedirect("/");
+
+                        }
+                        else
+                            return LocalRedirect("/login/Invalid credentials");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return LocalRedirect("/login/Invalid credentials");
+            }
+        }
+
+        [HttpGet("/account/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect("/");
+        }
+    }
+
+    public class UserCredentials
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
+
---
+++ BlzLogin/Startup.cs
@@ -0,0 +1,65 @@
+using BlzLogin.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BlzLogin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            services.AddControllers();  //Para poder añadir y gestionar controladores
+            services                    //Servicio de autenticación de ASP.NET 
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();           //Vamos a usar cookies para manetener el estado de autenticación
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //Midlewares para gestionar la autenticación y la autorización
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapControllers();     //Para que se mapeen los controladores y las rutas funcionen
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
